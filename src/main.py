566cb1a18dd00ce4150a9c2e901982f9cd45a3ca
phil616 <phil616@163.com>
2023-11-30 20:02:51 +0800

    Update feature in cachedb in order to access cachedb like a local dict variable i override the __getitem__ and __setitem__ methods

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 """
 you can add some example code here
 """
+from io_tools.cache import CacheObject
+
+cache = CacheObject('cache.json')
+
+def test():
+    cache['rom'] = 'kcc'
+    print(cache['rom'])
+
+if __name__ == "__main__":
+    test()
+    
